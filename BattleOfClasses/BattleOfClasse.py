7b4f3d9e9fbe2728bef5c6af7843efc4ce3a7c47
Robert Vári <mail.robertvari@gmail.com>
2022-06-19 15:41:02 +0200

    update

--- BattleOfClasses/BattleOfClasse.py
+++ BattleOfClasses/BattleOfClasse.py
@@ -4,6 +4,7 @@ import os
 class BattleOfClasses:
     def __init__(self):
         self._intro()
+        self._main_menu()
 
     def _intro(self):
         self._clear_screen()
@@ -17,6 +18,23 @@ class BattleOfClasses:
     @staticmethod
     def _clear_screen():
         os.system("cls" if os.name == "nt" else "clear")
+
+    def _main_menu(self):
+        print(f"Wellcome in this small town PLAYER_NAME")
+        print("There is a tavern on the right and an arena on the left.")
+        print("Where do you want to go?")
+        print("1. Black Horse tavern")
+        print("2. Arena")
+        print("3. Exit game")
+
+        player_input = input()
+
+        if player_input == "1":
+            print("Go to Black Horse tavern...")
+        elif player_input == "2":
+            print("Go to the Arena")
+        else:
+            self._exit()
 
 
 BattleOfClasses()
