c8c94eb9e48e0f88eb093184b726a3b9a25b6785
Robert Vári <mail.robertvari@gmail.com>
2022-06-19 16:28:56 +0200

    update

--- BattleOfClasses/game_assets/places.py
+++ BattleOfClasses/game_assets/places.py
@@ -7,10 +7,33 @@ class PlaceBase:
         self._player = player
         print(f"Wellcome in the {self._name} {self._player}")
 
+        self._main_menu()
+
+    def _main_menu(self):
+        print("PlaceBase _main_menu")
+
 
 class Tavern(PlaceBase):
-    pass
+    def _main_menu(self):
+        print("1. Buy something")
+        print("2. Go back to the street.")
+
+        user_input = input()
+
+        if user_input == "1":
+            print("TODO Buy something...")
+        else:
+            print("TODO Go back to main menu")
 
 
 class Arena(PlaceBase):
-    pass
+    def _main_menu(self):
+        print("1. Challenge yourself!")
+        print("2. Go back to the street.")
+
+        user_input = input()
+
+        if user_input == "1":
+            print("TODO Start challenge...")
+        else:
+            print("TODO Go back to main menu")
