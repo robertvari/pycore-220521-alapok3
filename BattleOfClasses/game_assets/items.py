0acc5195f48d0f06fa9344a9addc59f703a2dcb9
Robert Vári <mail.robertvari@gmail.com>
2022-06-19 17:02:40 +0200

    update

--- BattleOfClasses/game_assets/items.py
+++ BattleOfClasses/game_assets/items.py
@@ -3,9 +3,37 @@ class ItemBase:
         self._name = name
         self._price = price
         self._attr_modifier = attr_modifier
-        
+
     def __repr__(self):
         return self._name
 
     def __str__(self):
-        return f"Name: {self._name}\nPrice: {self._price}\nAttr Modifier: {self._attr_modifier}"
+        return f"{self._name} Price: {self._price} Attr Modifier: {self._attr_modifier}"
+
+
+class CommonItem(ItemBase):
+    pass
+
+
+class Weapon(ItemBase):
+    pass
+
+
+if __name__ == '__main__':
+    bread = CommonItem(name="Bread", price=5, attr_modifier=10)
+    milk = CommonItem("Milk", 10, 20)
+    cheese = CommonItem("Cheese", 30, 50)
+
+    hammer = Weapon("Hammer", 40, 30)
+    sword = Weapon("Sword", 10, 10)
+
+    shop_items = [
+        bread,
+        milk,
+        cheese,
+        hammer,
+        sword
+    ]
+
+    for index, i in enumerate(shop_items):
+        print(index, i)
