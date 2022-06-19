e940b7bb3020358805db1eb02ac0d67a80cf5e67
Robert Vári <mail.robertvari@gmail.com>
2022-06-19 17:46:11 +0200

    update

--- BattleOfClasses/game_assets/characters.py
+++ BattleOfClasses/game_assets/characters.py
@@ -1,10 +1,11 @@
 import random
-from game_assets.items import Weapon
+from game_assets.items import Weapon, CommonItem
 
 
 class CharacterBase:
     def __init__(self):
         self._name = None
+
         # todo add attributes here...
         self._golds = random.randint(2, 20)
         self._inventory = []
@@ -59,15 +60,29 @@ class Player(CharacterBase):
 
 
 class AIPlayer(CharacterBase):
-    pass
+    def _create(self):
+        super(AIPlayer, self)._create()
+
+        item_data = [
+            {"type": "CommonItem", "name": "Bread", "price": 10, "attr_modifier": 5},
+            {"type": "CommonItem", "name": "Milk", "price": 20, "attr_modifier": 13},
+            {"type": "CommonItem", "name": "Cheese", "price": 34, "attr_modifier": 40},
+            {"type": "CommonItem", "name": "Meat", "price": 40, "attr_modifier": 60},
+            {"type": "Weapon", "name": "Hammer", "price": 24, "attr_modifier": 30},
+            {"type": "Weapon", "name": "Sword", "price": 12, "attr_modifier": 15},
+            {"type": "Weapon", "name": "Dagger", "price": 20, "attr_modifier": 23},
+            {"type": "Weapon", "name": "Spear", "price": 34, "attr_modifier": 40},
+        ]
+
+        chosen_item = random.choice(item_data)
+        if chosen_item["type"] == "Weapon":
+            self._right_hand = Weapon(chosen_item["name"], chosen_item["price"], chosen_item["attr_modifier"])
+        else:
+            self._inventory.append(CommonItem(chosen_item["name"], chosen_item["price"], chosen_item["attr_modifier"]))
 
 
 if __name__ == '__main__':
-    player = Player()
-    sword = Weapon("Sword", 10, 20)
-    hammer = Weapon("Hammer", 10, 20)
-
-    player.buy(sword)
-    player.buy(hammer)
-
-    pass
+    enemy1 = AIPlayer()
+    enemy2 = AIPlayer()
+    enemy3 = AIPlayer()
+    enemy4 = AIPlayer()
