3b54a5d5ec589ce542ce4dfb429d1184ff5d07f4
Robert Vári <mail.robertvari@gmail.com>
2022-06-19 16:01:58 +0200

    update

---
+++ BattleOfClasses/game_assets/places.py
@@ -0,0 +1,12 @@
+class PlaceBase:
+    def __init__(self, name):
+        self._name = name
+        self._player = None
+
+
+class Tavern(PlaceBase):
+    pass
+
+
+class Arena(PlaceBase):
+    pass
